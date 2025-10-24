795962940bc3d1057749d72d566ac260366258d6
x3kristx <kristo.lember2@gmail.com>
2025-10-24 14:09:49 +0300

    Add author retrieval and update HTML structure in book.php

--- book.php
+++ book.php
@@ -13,4 +13,32 @@ $stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
+$stmt = pdo->prepare('SELECT first_name, last_name FROM book_authors ba  LEFT JOIN authors a ON ba.authors = :id');
+$stmt->execute(['book_id' => $id]);
+$authors = $stmt->fetchAll();
+
+// var_dump($authors);
+
 var_dump($book);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $book['title'];?></title>
+</head>
+<body>
+    <h1><?= $book ['title'] ?> </h1>
+
+    Autorid:
+    <ul>
+        <?php foreach ( $$variables as $key ) { ?>
+            # code ... 
+        <?php } ?>
+    </ul>
+    <h1>Pealkiri</h1>
+</body>
+</html>
